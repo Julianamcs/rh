e71449860112d37971f5099b31268d852e7424db
Juliana Maciel <julyanamcs@gmail.com>
2023-07-16 01:40:45 -0300

    Composição - Princípio de Substituição de Liskov
    tomar certos cuidados na hora de utilizar a herança para não criar efeitos colaterais na aplicação e a composição como uma alternativa.

--- src/br/com/alura/rh/model/Terceirizado.java
+++ src/br/com/alura/rh/model/Terceirizado.java
@@ -2,15 +2,16 @@ package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
 
-public class Terceirizado extends Funcionario{
+public class Terceirizado {
 
+	private DadosPessoais dadosPessoais;
 	private String empresa;
 	
-	public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-		super(nome, cpf, cargo, salario);
-		// TODO Auto-generated constructor stub
+	public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario, String empresa) {
+		this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+		this.empresa = empresa;
 	}
-
+	
 	public String getEmpresa() {
 		return empresa;
 	}
@@ -18,7 +19,23 @@ public class Terceirizado extends Funcionario{
 	public void setEmpresa(String empresa) {
 		this.empresa = empresa;
 	}
-	
+
+	public String getNome() {
+		return dadosPessoais.getNome();
+	}
+
+	public String getCpf() {
+		return dadosPessoais.getCpf();
+	}
+
+	public Cargo getCargo() {
+		return dadosPessoais.getCargo();
+	}
+
+	public BigDecimal getSalario() {
+		return dadosPessoais.getSalario();
+	}
+
 	
 
 }
