943a22747edfc80baeca1ee89d5518567a72d9ba
Juliana Maciel <julyanamcs@gmail.com>
2023-07-16 02:10:21 -0300

    segregação de interface
    no caso o "Anuenio" e ela está sendo obrigada a implementar um método que não faz sentido. Nós estamos implementando uma interface e precisa adicionar um novo método nessa interface, só que esse método não faz sentido para todas as classes que estão implementando.

---
+++ src/br/com/alura/service/tributacao/Reajuste.java
@@ -0,0 +1,11 @@
+package br.com.alura.service.tributacao;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public interface Reajuste {
+	
+	BigDecimal valor();
+	LocalDate data();
+
+}
